b225634b767cce4fb3fd2c69220f006215a51aa5
Kaye Holanda <kaye.holanda@gmail.com>
2022-03-30 17:19:14 -0300

    fix path on script

--- monit.sh
+++ monit.sh
@@ -4,4 +4,4 @@ target_path=result.txt
 echo "-----------" >> $target_path
 date >> $target_path
 echo "-----------" >> $target_path 
-speedtest-cli --simple >> $target_path 
+/usr/bin/speedtest-cli --simple >> $target_path 
